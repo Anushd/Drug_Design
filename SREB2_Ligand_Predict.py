c670bf8761a5a4f941dbf0ca93352b68e71a468c
Anush Devv <devadhasan.a@husky.neu.edu>
2017-06-18 01:50:14 +0530

    Update SREB2_Ligand_Predict.py

--- SREB2_Ligand_Predict.py
+++ SREB2_Ligand_Predict.py
@@ -7,30 +7,36 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from decimal import *
 from itertools import product, combinations
+import os
 
 #Set precision of decimal calulations
 getcontext().prec = 5
 
 ###Import_Structures###
-structure1 = open("/Users/anush/Desktop/Final_Ligands/713182_out.pdb", "r")
-structure2 = open("/Users/anush/Desktop/Final_Ligands/6605027_out.pdb", "r")
-structure3 = open("/Users/anush/Desktop/Final_Ligands/5389521_out.pdb", "r")
-structure4 = open("/Users/anush/Desktop/Final_Ligands/3247284_out.pdb", "r")
-structure5 = open("/Users/anush/Desktop/Final_Ligands/3236943_out.pdb", "r")
-structure6 = open("/Users/anush/Desktop/Final_Ligands/3236517_out.pdb", "r")
-structure7 = open("/Users/anush/Desktop/Final_Ligands/654602_out.pdb", "r")
-structure8 = open("/Users/anush/Desktop/Final_Ligands/653221_out.pdb", "r")
-structure9 = open("/Users/anush/Desktop/Final_Ligands/8548995_out.pdb", "r")
-structure10 = open("/Users/anush/Desktop/Final_Ligands/49786375_out.pdb", "r")
-structure11 = open("/Users/anush/Desktop/Final_Ligands/4911035_out.pdb", "r")
-structure12 = open("/Users/anush/Desktop/Final_Ligands/3247293_out.pdb", "r")
-structure13 = open("/Users/anush/Desktop/Final_Ligands/3147229_out.pdb", "r")
-structure14 = open("/Users/anush/Desktop/Final_Ligands/2982527_out.pdb", "r")
-structure15 = open("/Users/anush/Desktop/Final_Ligands/2508673_out.pdb", "r")
-structure16 = open("/Users/anush/Desktop/Final_Ligands/16231644_out.pdb", "r")
-structure17 = open("/Users/anush/Desktop/Final_Ligands/16031807_out.pdb", "r")
+path = '/Users/anush/Desktop/Final_Ligands/'
 
+structures_min = []
+count=0
+for i in os.listdir(path):
+	if count != 0:
+		file1 = open("/Users/anush/Desktop/Final_Ligands/"+i, "r")
+		structures_min.append(file1)
+	count += 1
+
+structures = []	
+count=0
+for i in os.listdir(path):
+	if count != 0:
+		file2 = open("/Users/anush/Desktop/Final_Ligands/"+i, "r")
+		structures.append(file2)
+	count += 1
+count =0
+
+"""
 structures_min = [open("/Users/anush/Desktop/Final_Ligands/713182_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/6605027_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/5389521_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/3247284_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/3236943_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/3236517_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/654602_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/653221_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/8548995_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/49786375_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/4911035_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/3247293_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/3147229_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/2982527_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/2508673_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/16231644_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/16031807_out.pdb", "r")]
+
+structures = [open("/Users/anush/Desktop/Final_Ligands/713182_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/6605027_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/5389521_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/3247284_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/3236943_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/3236517_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/654602_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/653221_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/8548995_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/49786375_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/4911035_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/3247293_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/3147229_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/2982527_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/2508673_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/16231644_out.pdb", "r"),open("/Users/anush/Desktop/Final_Ligands/16031807_out.pdb", "r")]"""
+
 
 #[binding affinity]
 data = [[-7.3],[-7.9],[-8.6],[-8.2],[-7.4],[-8.7],[-8.2],[-8.6],[-9.4],[-8.7],[-8.6],[-8.7],[-9.1],[-9.5],[-7.3],[-8.6],[-9.7]]
@@ -266,11 +272,14 @@ def Probability(models):
 
 ###SVM_Classification###
 def SVM(probs,data,quadrant,to_predict):
-	
-	label_set = array([probs[0][quadrant],probs[1][quadrant],probs[2][quadrant],probs[3][quadrant],probs[4][quadrant],probs[5][quadrant],probs[6][quadrant],probs[7][quadrant],probs[8][quadrant],probs[9][quadrant],probs[10][quadrant],probs[11][quadrant],probs[12][quadrant],probs[13][quadrant],probs[14][quadrant],probs[15][quadrant],probs[16][quadrant]])
+	label_set = []
+	count=0
+	for i in probs:
+		label_set.append(i[quadrant])
+		count+=1	
+	label_set = array(label_set)
 	data_set = array(data)
-	
-	clf = svm.SVR(kernel='rbf', gamma=1e-3,C=1e7)
+	clf = svm.SVR(kernel='rbf', gamma=1e-3,C=1e2)
 	clf.fit(data_set, label_set)  
 	
 	prediction = clf.predict([to_predict])
@@ -278,118 +287,40 @@ def SVM(probs,data,quadrant,to_predict):
 
 ###Run_Functions###		
 min = Min(structures_min)
-s1 = Coords(structure1)
-t1 = Translate(min[0], min[1], min[2], s1[0], s1[1], s1[2])
-s2 = Coords(structure2)
-t2 = Translate(min[0], min[1], min[2], s2[0], s2[1], s2[2])
-s3 = Coords(structure3)
-t3 = Translate(min[0], min[1], min[2], s3[0], s3[1], s3[2])
-s4 = Coords(structure4)
-t4 = Translate(min[0], min[1], min[2], s4[0], s4[1], s4[2])
-s5 = Coords(structure5)
-t5 = Translate(min[0], min[1], min[2], s5[0], s5[1], s5[2])
-s6 = Coords(structure6)
-t6 = Translate(min[0], min[1], min[2], s6[0], s6[1], s6[2])
-s7 = Coords(structure7)
-t7 = Translate(min[0], min[1], min[2], s7[0], s7[1], s7[2])
-s8 = Coords(structure8)
-t8 = Translate(min[0], min[1], min[2], s8[0], s8[1], s8[2])
-s9 = Coords(structure9)
-t9 = Translate(min[0], min[1], min[2], s9[0], s9[1], s9[2])
-s10 = Coords(structure10)
-t10 = Translate(min[0], min[1], min[2], s10[0], s10[1], s10[2])
-s11 = Coords(structure11)
-t11 = Translate(min[0], min[1], min[2], s11[0], s11[1], s11[2])
-s12 = Coords(structure12)
-t12 = Translate(min[0], min[1], min[2], s12[0], s12[1], s12[2])
-s13 = Coords(structure13)
-t13 = Translate(min[0], min[1], min[2], s13[0], s13[1], s13[2])
-s14 = Coords(structure14)
-t14 = Translate(min[0], min[1], min[2], s14[0], s14[1], s14[2])
-s15 = Coords(structure15)
-t15 = Translate(min[0], min[1], min[2], s15[0], s15[1], s15[2])
-s16 = Coords(structure16)
-t16 = Translate(min[0], min[1], min[2], s16[0], s16[1], s16[2])
-s17 = Coords(structure17)
-t17 = Translate(min[0], min[1], min[2], s17[0], s17[1], s17[2])
 
-structures_max = [t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17]
+s = []
+structures_max = []
+count=0
+for i in structures:
+	s.append(Coords(i))
+	structures_max.append(Translate(min[0], min[1], min[2], (s[count])[0], (s[count])[1], (s[count])[2]))
+	count+=1
 
-max1 = Max(structures_max)
-max2 = max1
-max3 = max1
-max4 = max1
-max5 = max1
-max6 = max1
-max7 = max1
-max8 = max1
-max9 = max1
-max10 = max1
-max11 = max1
-max12 = max1
-max13 = max1
-max14 = max1
-max15 = max1
-max16 = max1
-max17 = max1
-model1 = Construct(t1[0], t1[1], t1[2], max1)
-model2 = Construct(t2[0], t2[1], t2[2], max2)
-model3 = Construct(t3[0], t3[1], t3[2], max3)
-model4 = Construct(t4[0], t4[1], t4[2], max4)
-model5 = Construct(t5[0], t5[1], t5[2], max5)
-model6 = Construct(t6[0], t6[1], t6[2], max6)
-model7 = Construct(t7[0], t7[1], t7[2], max7)
-model8 = Construct(t8[0], t8[1], t8[2], max8)
-model9 = Construct(t9[0], t9[1], t9[2], max9)
-model10 = Construct(t10[0], t10[1], t10[2], max10)
-model11 = Construct(t11[0], t11[1], t11[2], max11)
-model12 = Construct(t12[0], t12[1], t12[2], max12)
-model13 = Construct(t13[0], t13[1], t13[2], max13)
-model14 = Construct(t14[0], t14[1], t14[2], max14)
-model15 = Construct(t15[0], t15[1], t15[2], max15)
-model16 = Construct(t16[0], t16[1], t16[2], max16)
-model17 = Construct(t17[0], t17[1], t17[2], max17)
+maxs = []
+count=0
+for i in structures_max:
+	maxs.append(Max(structures_max))
+	count+=1
 
-partition1 = Partition(model1[0])
-partition2 = Partition(model2[0])
-partition3 = Partition(model3[0])
-partition4 = Partition(model4[0])
-partition5 = Partition(model5[0])
-partition6 = Partition(model6[0])
-partition7 = Partition(model7[0])
-partition8 = Partition(model8[0])
-partition9 = Partition(model9[0])
-partition10 = Partition(model10[0])
-partition11 = Partition(model11[0])
-partition12 = Partition(model12[0])
-partition13 = Partition(model13[0])
-partition14 = Partition(model14[0])
-partition15 = Partition(model15[0])
-partition16 = Partition(model16[0])
-partition17 = Partition(model17[0])
+models = []
+count=0
+for i in structures_max:
+	models.append(Construct(i[0],i[1],i[2],maxs[count]))
+	count+=1
 
-prob1 = Probability(partition1)
-prob2 = Probability(partition2)
-prob3 = Probability(partition3)
-prob4 = Probability(partition4)
-prob5 = Probability(partition5)
-prob6 = Probability(partition6)
-prob7 = Probability(partition7)
-prob8 = Probability(partition8)
-prob9 = Probability(partition9)
-prob10 = Probability(partition10)
-prob11 = Probability(partition11)
-prob12 = Probability(partition12)
-prob13 = Probability(partition13)
-prob14 = Probability(partition14)
-prob15 = Probability(partition15)
-prob16 = Probability(partition16)
-prob17 = Probability(partition17)
-
-probs = [prob1,prob2,prob3,prob4,prob5,prob6,prob7,prob8,prob9,prob10,prob11,prob12,prob13,prob14,prob15,prob16,prob17]
+partitions = []
+count=0
+for i in models:
+	partitions.append(Partition(i[0]))
+	count+=1
+probs = []
+count=0
+for i in partitions:
+	probs.append(Probability(i))
+	count+=1
 
 quadrant = [0,1,2,3,4,5,6,7]
-to_predict = -6.0
+to_predict = -8.0
 
 finalA = []
 for i in quadrant:
@@ -403,69 +334,58 @@ print
 print "Second Iteration:"
 print 
 
-#Second iteration on quadrant 5
+#Second iteration 
 quad_count=0
 final_graph=[]
 for i in quadrant:
 	print "Quadrant:"
 	print i+1
 	print
-	partition1b = Partition(partition1[i])
-	partition2b = Partition(partition2[i])
-	partition3b = Partition(partition3[i])
-	partition4b = Partition(partition4[i])
-	partition5b = Partition(partition5[i])
-	partition6b = Partition(partition6[i])
-	partition7b = Partition(partition7[i])
-	partition8b = Partition(partition8[i])
-	partition9b = Partition(partition9[i])
-	partition10b = Partition(partition10[i])
-	partition11b = Partition(partition11[i])
-	partition12b = Partition(partition12[i])
-	partition13b = Partition(partition13[i])
-	partition14b = Partition(partition14[i])
-	partition15b = Partition(partition15[i])
-	partition16b = Partition(partition16[i])
-	partition17b = Partition(partition17[i])
-
-	prob1b = Probability(partition1b)
-	prob2b = Probability(partition2b)
-	prob3b = Probability(partition3b)
-	prob4b = Probability(partition4b)
-	prob5b = Probability(partition5b)
-	prob6b = Probability(partition6b)
-	prob7b = Probability(partition7b)
-	prob8b = Probability(partition8b)
-	prob9b = Probability(partition9b)
-	prob10b = Probability(partition10b)
-	prob11b = Probability(partition11b)
-	prob12b = Probability(partition12b)
-	prob13b = Probability(partition13b)
-	prob14b = Probability(partition14b)
-	prob15b = Probability(partition15b)
-	prob16b = Probability(partition16b)
-	prob17b = Probability(partition17b)
 	
-	probsb = [prob1b,prob2b,prob3b,prob4b,prob5b,prob6b,prob7b,prob8b,prob9b,prob10b,prob11b,prob12b,prob13b,prob14b,prob15b,prob16b,prob17b]	
+	partitionsb = []
+	count=0
+	for j in partitions:
+		partitionsb.append(Partition(j[i]))
+		count+=1
+	
+	probsb = []
+	count=0
+	for j in partitionsb:
+		probsb.append(Probability(j))
+		count+=1
 
 	finalB = []
-	for i in quadrant:
-		finalB.append(SVM(probsb,data,i,to_predict))
+	for l in quadrant:
+		finalB.append(SVM(probsb,data,l,to_predict))
 	final_graph.append(finalB)
 	print "Predicted:"
 	print finalB
 	print
-	print 
+	print
 
 '''fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(model1[1], model1[2], model1[3])
 plt.show()'''
+'''
+x=0
+for i in final_graph:
+	y=min(0,5,3,2)
+	z=0
+	for l in i:
+		if y<0:
+			final_graph[x][z]+=abs(y)
+		z+=1
+	x+=1	
+print
+print final_graph'''
 
 ###Plot_Probabilities###
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.set_aspect("equal")
+ax.grid(True)
+ax.set_axis_on()
 
 #Quadrant_1
 u, v = np.mgrid[0:2*np.pi:10j, 0:np.pi:10j]
